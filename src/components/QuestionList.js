e1d6c69a6c0980f19a781171ed958824467e7903
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-10 16:09:02 +0300

    Setting our useState and UseEffect

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,6 +2,13 @@ import React, {useEffect , useState} from "react";
 import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions,setQuestions] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(questions => setQuestions(questions))
+  },[])
   return (
     <section>
       <h1>Quiz Questions</h1>
