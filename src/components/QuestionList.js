2195f2cf400de9697be63d73e424aec432ab14d8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-10 16:25:24 +0300

    calling our questionItems variable

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -56,7 +56,7 @@ function QuestionList() {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
